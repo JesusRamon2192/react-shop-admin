1b8577b9bfd2b84393c43d7d4c1b5eb94a47af21
jesusRamon <jesus.ramon2192@gmail.com>
2023-03-13 18:01:31 -0600

    Apunntes

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -25,16 +25,18 @@ function useProvideAuth() {
         'Content-Type': 'application/json',
       },
     };
+    //Lee un access token que regresa desde la información del servidor
     const { data: access_token } = await axios.post(endPoints.auth.login, { email, password }, options);
-
-    console.log(access_token);
+    //console.log(access_token); //Nos permite ver la información retornada
     if (access_token) {
-      const token = access_token.access_token;
-      Cookie.set('token', token.access_token, {
-        expires: 5,
-      });
+      const token = access_token.access_token; //requerido para el acceso a la información
+      Cookie.set('token', token, { expires: 5 });
+      /*expires permite que después de un tiempo definido podamos eliminar
+	    la información almacenada y pueda volver a logear*/
+      //Se envía la información necesaria para que pueda definir el valor por defecto
       axios.defaults.headers.Authorization = `Bearer ${token}`;
-      const {data: user} = await axios.get(endPoints.auth.profile);
+      //Vamos a llamar el recurso con el profile
+      const { data: user } = await axios.get(endPoints.auth.profile);
       console.log(user);
       setUser(user);
     }
